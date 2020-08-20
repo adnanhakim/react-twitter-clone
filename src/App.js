a69ca35e0df619a5c7817a7374b539715bb1b350
Adnan Hakim <adnanhakim007@gmail.com>
2020-08-21 01:25:24 +0530

    Feed layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
+import Widgets from './components/Widgets';
 
 function App() {
    return (
       <div className="app">
          <Sidebar />
-
          <Feed />
+         <Widgets />
       </div>
    );
 }
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,8 +3,10 @@ import './Feed.css';
 
 function Feed() {
    return (
-      <div>
-         <h2>Home</h2>
+      <div className="feed">
+         <div className="feed-header">
+            <h2>Home</h2>
+         </div>
       </div>
    );
 }
