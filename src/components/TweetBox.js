c8c88a84ac9a26427177b560f1abd787fcc0afec
Adnan Hakim <adnanhakim007@gmail.com>
2020-08-22 02:03:03 +0530

    Tweets working

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TweetBox.css';
 import { Avatar, Button } from '@material-ui/core';
+import db from '../firebase';
 
 function TweetBox() {
+   const [tweetMessage, setTweetMessage] = useState('');
+   const [tweetImage, setTweetImage] = useState('');
+
+   const sendTweet = (e) => {
+      e.preventDefault();
+
+      db.collection('posts').add({
+         displayName: 'Adnan Hakim',
+         username: 'adnanhakim',
+         verified: true,
+         text: tweetMessage,
+         image: tweetImage,
+         avatar:
+            'https://avatars1.githubusercontent.com/u/46572622?s=460&u=b7bd8dd34eabcf10a7d9f10ed10e3786467d7db4&v=4',
+      });
+
+      setTweetMessage('');
+      setTweetImage('');
+   };
+
    return (
       <div className="tweetbox">
          <form>
             <div className="tweetbox-input">
                <Avatar src="https://avatars1.githubusercontent.com/u/46572622?s=460&u=b7bd8dd34eabcf10a7d9f10ed10e3786467d7db4&v=4" />
-               <input type="text" placeholder="What's happening?" />
+               <input
+                  onChange={(e) => setTweetMessage(e.target.value)}
+                  value={tweetMessage}
+                  type="text"
+                  placeholder="What's happening?"
+               />
             </div>
             <input
+               onChange={(e) => setTweetImage(e.target.value)}
+               value={tweetImage}
                className="tweetbox-image-input"
                type="text"
                placeholder="Optional: Enter image URL"
             />
-            <Button className="tweetbox-tweet-button">Tweet</Button>
+            <Button
+               onClick={sendTweet}
+               type="submit"
+               className="tweetbox-tweet-button">
+               Tweet
+            </Button>
          </form>
       </div>
    );
